67648b23f925e3067206a651a0c70b46f2bf1bb7
Tomáš Novotný <84340580+tomasnovotnyy@users.noreply.github.com>
2024-02-24 19:45:58 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,18 +2,18 @@ import threading
 import signal
 import sys
 
-from UDP import UDP
+from Communication import Communication
 
 
 def cleanup_and_exit():
     """
     Uzavře všechny otevřené sokety a ukončí program.
     """
-    if UDP.udp_socket is not None:
-        UDP.udp_socket.close()
-    if UDP.tcp_server_socket is not None:
-        UDP.tcp_server_socket.close()
-    for client_socket in UDP.tcp_client_sockets:
+    if Communication.udp_socket is not None:
+        Communication.udp_socket.close()
+    if Communication.tcp_server_socket is not None:
+        Communication.tcp_server_socket.close()
+    for client_socket in Communication.tcp_client_sockets:
         if client_socket is not None:
             client_socket.close()
     sys.exit(0)
@@ -26,20 +26,20 @@ def main():
     """
     signal.signal(signal.SIGINT, lambda signal, frame: cleanup_and_exit())
 
-    UDP.start_udp_listener()
-    UDP.periodic_udp_discovery()
+    Communication.start_udp_listener()
+    Communication.periodic_udp_discovery()
 
-    tcp_server_thread = threading.Thread(target=UDP.start_tcp_server)
+    tcp_server_thread = threading.Thread(target=Communication.start_tcp_server)
     tcp_server_thread.start()
 
-    for peer_ip in UDP.discovered_peers:
-        tcp_client_thread = threading.Thread(target=UDP.start_tcp_client, args=(peer_ip,))
+    for peer_ip in Communication.discovered_peers:
+        tcp_client_thread = threading.Thread(target=Communication.start_tcp_client, args=(peer_ip,))
         tcp_client_thread.start()
 
-    send_thread = threading.Thread(target=UDP.send_messages)
+    send_thread = threading.Thread(target=Communication.send_messages)
     send_thread.start()
 
-    receive_thread = threading.Thread(target=UDP.receive_messages)
+    receive_thread = threading.Thread(target=Communication.receive_messages)
     receive_thread.start()
 
     while True:
